160d5ae2ba69fce6a0f504b49b70f6d5c0ae989a
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-03 12:01:55 +0530

    Create 73.java

---
+++ SET8/73.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class between
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		if(a<n&&b>n)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
