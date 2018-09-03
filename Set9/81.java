ffc1a58b66caaafe1026d637c31b612aa315eea2
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-03 12:44:14 +0530

    Create 81.java

---
+++ Set9/81.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Set81 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		if(a<b)
+	       System.out.print(b-a);
+		else
+			System.out.print("invalid no");
+	}
+}
