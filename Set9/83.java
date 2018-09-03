c78848bd1b650021776d0c580f353eafa9853b26
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-03 13:01:51 +0530

    Create 83.java

---
+++ Set9/83.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class opp{
+
+     public static void main(String aa[]){
+     int a;
+     int b;
+     int y;
+     char c;
+     Scanner in=new Scanner(System.in);
+     a=s.nextInt();
+     c=s.next().charAt(0);
+       b=s.nextInt();
+   switch(c){
+       case '+':
+            y=a+b;
+           System.out.println(y);
+           break;
+           case '-':
+            y=a-b;
+           System.out.println(y);
+           break;
+           case '/':
+            y=a/b;
+           System.out.println(y);
+           break;
+           case '%':
+           y=a%b;
+           System.out.println(y);
+           break;
+           
+   }
+}
+}
