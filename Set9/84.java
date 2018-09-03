db1b57b2e1dd1fb90897190f91ce8f5bc83151b6
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-03 13:04:10 +0530

    Create 84.java

---
+++ Set9/84.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Set84 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		char ch=in.next().charAt(0);
+		if((ch>=65 && ch<=90)||(ch>=97 && ch<=122)||(ch>=48 && ch<=57))
+			System.out.print((int)ch);
+	}
+}
