1f2e22a71815da81f6a53caf15e68ab58e7530a5
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-03 13:15:20 +0530

    Create 85.java

---
+++ Set9/85.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class Set85 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=in.nextLine();
+		s=s.toUpperCase();
+		String s1[]=s.split("");
+		String a="",b="";
+		for(int i=0;i<s1.length;i++){
+				if(i%2==0)
+					a=a+s1[i];
+				else
+					b=b+s1[i];
+		}
+		System.out.print(a+" "+b);
+	}
+}
