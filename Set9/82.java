81f11b5c3c6271dd82b825a7a60d0161dd88f542
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-03 12:51:16 +0530

    Create 82.java

---
+++ Set9/82.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		double a,b,c;
+		a=sc.nextDouble();
+		b=sc.nextDouble();
+		c=a*b;
+		System.out.print("%.5f",c);
+	}
+}
+
+
