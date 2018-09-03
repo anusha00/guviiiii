4e5814995a228a40ca642fa149bbcd72af62e621
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-03 12:26:00 +0530

    Rename 77.java to factors.java

---
+++ SET8/factors.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ class sdr
+ {
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) 
+		{
+			if(n%i==2)
+			{
+				System.out.println(i+" ");
+			}
+		
+	}
+  }
+}	            
