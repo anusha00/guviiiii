88bdb89a15638dca398ad266f199a3f2424a43bc
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-03 12:06:05 +0530

    Create 74.java

---
+++ SET8/74.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Roundoff {
+	public static void main(String s[]){
+		Scanner in=new Scanner(System.in);
+		float a=scanner.nextFloat();
+		double b=Math.ceil(a);
+		int c=(int)b;
+		System.out.print(c);
+		
+		
+	}
+}
